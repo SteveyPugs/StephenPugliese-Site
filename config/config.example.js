3f2db9fbd3eeea5c8a7caeafab1232f55ec26605
Stephen Pugliese <stephen.pugliese@outlook.com>
2015-05-22 21:53:29 -0400

    Fixes #3

--- config/config.example.js
+++
@@ -1,8 +0,0 @@
-//config file to hold github login details
-
-exports.config = {
-	github_username: 'github_username',
-	github_password: 'github_password',
-	hostname: 'localhost',
-	port: 8080,
-};
---
+++ config/index.example.js
@@ -0,0 +1,8 @@
+var config = {
+	github_username: "github_username",
+	github_password: "github_password",
+	hostname: "localhost",
+	port: 8080,
+};
+
+exports.config = config;
